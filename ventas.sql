d01f177d63c7112cd4c79c47b9fb8e7026418a41
anrvelez-star <anrvelez@gmail.com>
2026-03-09 05:57:17 -0500

    Add ventas.sql file with sales transactions

---
+++ ventas.sql
@@ -0,0 +1,15 @@
+INSERT INTO sales (invoice_number, client, product, quantity, date, payment_method) VALUES 
+('INV001', 'Client A', 'Product X', 2, '2026-03-09', 'Credit Card'),
+('INV002', 'Client B', 'Product Y', 1, '2026-03-09', 'Cash'),
+('INV003', 'Client C', 'Product Z', 5, '2026-03-09', 'Paypal'),
+('INV004', 'Client D', 'Product W', 3, '2026-03-09', 'Transfer'),
+('INV005', 'Client E', 'Product V', 2, '2026-03-09', 'Debit Card'),
+('INV006', 'Client F', 'Product U', 4, '2026-03-09', 'Credit Card'),
+('INV007', 'Client G', 'Product T', 1, '2026-03-09', 'Cash'),
+('INV008', 'Client H', 'Product S', 6, '2026-03-09', 'Bank Transfer'),
+('INV009', 'Client I', 'Product R', 2, '2026-03-09', 'Paypal'),
+('INV010', 'Client J', 'Product Q', 1, '2026-03-09', 'Credit Card');
+----
+... up to 100 transactions ...
+----
+('INV100', 'Client Z', 'Product A', 3, '2026-03-09', 'Bank Transfer');
