853a8f808ad52a542e300a4f755ba276013dcd62
anrvelez-star <anrvelez@gmail.com>
2026-03-09 06:03:49 -0500

    Create productos.sql file with product data

---
+++ productos.sql
@@ -0,0 +1,9 @@
+-- Tabla de Productos Madecentro
+INSERT INTO Productos (id_producto, nombre, marca, categoria, precio_sin_descuento, precio_con_descuento, porcentaje_descuento) VALUES
+(1, 'Horno empotrable electrico', 'Haceb', 'Electrodomésticos', 950000, 790000, '-17%'),
+(2, 'Griferia monocontrol cocina', 'Corona', 'Grifería', 189000, 145000, '-23%'),
+(3, 'Estufa empotrable 4 puestos', 'Haceb', 'Electrodomésticos', 650000, 520000, '-20%'),
+(4, 'Lavaplatos acero inoxidable 60 cm', 'Haceb', 'Electrodomésticos', 230000, 186000, '-19%'),
+(5, 'Campana extractora acero inoxidable', 'Haceb', 'Electrodomésticos', 520000, 410000, '-21%'),
+(6, 'Campana Tofu color Negro de 3 Velocidades', 'Haceb', 'Electrodomésticos', 329900, 239900, '-27%'),
+(7, 'Horno electrico medio formato 60 cm inoxidable 120v', 'Kalley', 'Electrodomésticos', 1031900, 862900, '-16%');
