96f39843707ada081374a9733465a57ef2dc547d
anrvelez-star <anrvelez@gmail.com>
2026-03-09 06:16:29 -0500

    Create ventas_completa.sql with INSERT INTO Ventas statements for 100 transactions

---
+++ ventas_completa.sql
@@ -0,0 +1,4 @@
+-- Insert statements for ventas_completa.sql file
+
+INSERT INTO Ventas (id_venta, numero_factura, id_producto, id_cliente, cantidad, precio_unitario, fecha, forma_pago, sede_madecentro)
+VALUES
